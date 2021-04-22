2719692544aa7c5870981054185de4cf854901c5
AnkitVasita <vasitaankit97@gmail.com>
2021-04-23 01:18:44 +0530

    changes

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API Key - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "AIzaSyB085YLaSt4YhcdKkjGQWWh6_kD2lzRtzo";
+
+// Context Key- https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "1dd07c81052988d1a";
